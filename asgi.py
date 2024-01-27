148d87b65e3b07a8bbe97f063a9453038097def8
shubham97s <157363750+shubham97s@users.noreply.github.com>
2024-01-27 22:55:31 +0530

    Add files via upload

---
+++ asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Insertapp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Insertapp.settings')
+
+application = get_asgi_application()
